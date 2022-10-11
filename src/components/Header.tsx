c9655b17d28420976ab60073fe7d9d35e6571786
Renan Moreira <renan.tsx@gmail.com>
2022-10-11 15:37:27 -0300

    new src logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="/logo.svg" alt="to.do"/>
+        <img src="https://raw.githubusercontent.com/MyIgnite/to-do/e927771a49df18e1ca4a37f7bbfc9ede765d651b/public/logo.svg" alt="to.do"/>
       </div>
     </header>
   )
